b8d638643dd3f1bfd18ef2d7230e270ee47323de
drilon305 <drilonsulejmani305@gmail.com>
2022-09-07 19:34:27 +0200

    finished hero

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,10 +1,28 @@
-import './HeroStyle.css'
-
 import React from 'react'
+import './HeroStyle.css'
+import { AiOutlineSearch } from 'react-icons/ai'
+
+import Video from '../../assets/maldivesVideo.mp4'
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+        <video autoPlay loop muted id='video'>
+            <source src={Video} type='video/mp4' />
+        </video>
+        <div className="overlay"></div>
+        <div className="content">
+            <h1>First Class Travel</h1>
+            <h2>Top 1% Locations Worldwide</h2>
+            <form className='form'>
+                <div>
+                <input type="text" placeholder='Search Destanations' />
+                </div>
+                <button><AiOutlineSearch className='icon' /></button>
+            </form>
+        </div>
+    </div>
   )
 }
 
